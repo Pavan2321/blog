cdf7b4678bc6982aaef8b6fdd5f9296286508b38
Pavan Kumar (kit kat) <95262587+Pavan2321@users.noreply.github.com>
2024-08-24 17:33:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,14 +15,18 @@ app.use('/api/projects', projectRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-});
+app.get('/',(req, res)=>{
+    res.send("backend Dashbard")
+})
+
+// mongoose.connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// }).then(() => {
+//     console.log('Connected to MongoDB');
+// }).catch((error) => {
+//     console.error('Error connecting to MongoDB:', error);
+// });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
